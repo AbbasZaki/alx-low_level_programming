06d8c49275ece0fd0cdb47cc52c76a2837f2a026
AbbasZaki <you@example.com>
2022-12-08 13:28:47 -0800

    ok

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  *  * main - main block
  *   * Return: 0
@@ -8,4 +8,3 @@ int main(void)
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
